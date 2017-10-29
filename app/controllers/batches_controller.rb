dff1b23bce53adf241fd854168f4e7b1322ca933
Yechiel Kalmenson <contact@yechiel.me>
2017-10-28 21:26:50 -0400

    stub out uploading input and returning output

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,8 +1,14 @@
 class BatchesController < ApplicationController
   def index
     @batches = Batch.all
+    @batch = Batch.new
   end
 
   def create
+    @batch = Batch.create(input: params[:batch][:input].read)
+
+    @batch.parse_input
+
+    send_data @batch.output, filename: "output.txt"
   end
 end
--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,3 +1,8 @@
 class Batch < ActiveRecord::Base
     has_many :applicants
+
+    def parse_input
+        self.output = self.input
+        self.save
+    end
 end
